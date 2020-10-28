29042845f0c83e9807ac5561b2b27b6419ab30e8
mselmi <selmimarco@gmail.com>
2020-10-28 14:25:51 +0100

    Rinominato Test2Edit.java in TestEdit.java.
    test2-edit.xml reso modale.

--- modules/global/src/com/company/test/entity/Test2.java
+++ modules/global/src/com/company/test/entity/Test2.java
@@ -1,13 +1,8 @@
 package com.company.test.entity;
 
-import com.haulmont.cuba.core.entity.StandardEntity;
-import com.haulmont.cuba.core.entity.annotation.Extends;
-
 import javax.persistence.Entity;
-import javax.persistence.Table;
 
 @Entity(name = "test_Test2")
-@Extends(Test.class)
 public class Test2 extends Test {
     private static final long serialVersionUID = 4009089102651859791L;
 }
